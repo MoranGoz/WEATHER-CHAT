739a75c60812dadea65d18b62e77c0ae36cc2d72
bekihs <bekihs2003@gmail.com>
2018-05-08 15:21:28 +0300

    Feedback
    Just read, don't approve my changes.

--- public/api.js
+++ public/api.js
@@ -3,11 +3,11 @@ import {Comment} from './comment.js';
 import {currentWeb} from "./WeatherChatData.js";
 import {weatherrander} from './rander.js';
 
-var setData = function (city, data) {
+var setData = function (city, data) { // Tis function should be inside the class. No more wondering around function. All function should be inside a class/
     var temp = data.query.results.channel.item.condition.temp;
     var text = data.query.results.channel.item.condition.text;
     var date = data.query.results.channel.item.condition.date;
-    currentWeb.creatcity(city, temp, text, date);
+    currentWeb.creatcity(city, temp, text, date); // I prefer that the function willl return the value and wouldnt ust access the data layer.
  }
 class Api {
     constructor (){
@@ -18,8 +18,9 @@ class Api {
             method: "GET",
             url: "https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json",
             success: function (data) {
-                setData(city, data);
-                weatherrander.randerWeatherChat(currentWeb);
+                setData(city, data); // let obj = this.setData(city, data); return obj;
+                weatherrander.randerWeatherChat(currentWeb); /// The API shouln't know about the view layer and the data layer. Those two function should be invoked from the controller.
+                // For that to happen you should return a promise from this function. Use te then function an dnot the success.
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(textStatus);
