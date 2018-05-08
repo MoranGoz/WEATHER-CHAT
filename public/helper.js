40262e2ed176fbe9f91a7498fa53bf965c7fb757
bekihs <bekihs2003@gmail.com>
2018-05-08 15:43:41 +0300

    Update helper.js

--- public/helper.js
+++ public/helper.js
@@ -1,9 +1,12 @@
 import {City} from './city.js';
+// None of those are ever in use
 import {Comment} from './comment.js';
 import {currentWeb} from "./WeatherChatData.js";
 import {weatherApi} from "./api.js";
 import {weatherrander} from "./rander.js";
 
+
+/// What isthis file all about?
 class Helper{
     constructor (){
     }
